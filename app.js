97ecd778fd270d1d8d8b2cfaeab7a8ef160e35da
emilyleya <emily.fischer@study.thws.de>
2026-06-15 08:38:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -688,6 +688,7 @@ function isWorldWonder(site) {
            name.includes("chichen") ||             
            name.includes("colosseum") ||  
            name.includes("colloseo") ||
+           name.includes("colloseum")
            name.includes("taj mahal");             
 }
 
